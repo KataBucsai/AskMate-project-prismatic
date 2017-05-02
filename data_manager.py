0c71d06d134b4065655b5094649a59e43baf5c58
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-05-02 15:51:20 +0200

    timestamp, timestamp decoder, bytearray converter created

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,17 @@
 import base64
-
+import time
+import datetime
 
 # read file into a @table
 #
 # @file_name: string
 # @table: list of lists of strings
-def get_table_from_file(file_name):
+def get_table_from_file(file_name, indices):
     with open(file_name, "r") as file:
         lines = file.readlines()
     table = [element.replace("\n", "").split(",") for element in lines]
+    table = base64_decoder(table, indices)
+    table = get_timeform_from_stamp(table)
     return table
 
 
@@ -26,18 +29,31 @@ def write_table_to_file(file_name, table):
 def base64_decoder(table, indices):
     for row in table:
         for i in indices:
-            row[i] = base64.b64decode(row[i])
+            row[i] = base64.b64decode(row[i]).decode("utf-8")
     return table
 
 
 def base64_coder(table, indices):
     for row in table:
         for i in indices:
-            row[i] = base64.b64encode(row[i])
+            row[i] = base64.b64encode(bytearray(row[i].encode))
     return table
 
 
 def table_sort(table, sort_key_number, rev_boolean):
     table = sorted(table, key=lambda table: table[sort_key_number], reverse=rev_boolean)
     return table
+
+
+def get_time_stamp():
+    return int(time.time())
+
+
+def get_timeform_from_stamp(table):
+    for row in table:
+        row[1] = datetime.datetime.fromtimestamp(int(row[1])).strftime('%Y-%m-%d %H:%M:%S')
+    return table
+
+
+
 
--- main.py
+++ main.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def list_questions():
     file_name = current_file_path + "/data/question.csv"
-    question_list = data_manager.get_table_from_file(file_name)
-    question_list = data_manager.base64_decoder(question_list, (4, 5, 6))
+    question_list = data_manager.get_table_from_file(file_name, (4, 5, 6))
     question_list = data_manager.table_sort(question_list, 1, True)
     return render_template('list_questions.html', question_list=question_list)
 
